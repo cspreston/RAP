0789ce2047f0584700d89f2d76c49c4585d1cedb
javaday <javaday@gmail.com>
2017-01-28 08:27:24 -0700

    Updated migration batch

--- CTR.RAP.Migrations/migrate.bat
+++ CTR.RAP.Migrations/migrate.bat
@@ -15,4 +15,5 @@ SET migrator="%parent%..\packages\FluentMigrator.1.6.2\tools\migrate.exe"
 SET target="%parent%bin\%--env%\CTR.RAP.Migrations.dll"
 
 %migrator% --conn "Data Source=(local);Initial Catalog=RAP#Cleanup & Total Restoration;Integrated Security=true;" --provider sqlserver2014 --assembly %target% --task %--task% --verbose true
+%migrator% --conn "Data Source=(local);Initial Catalog=RAP#RAP Training;Integrated Security=true;" --provider sqlserver2014 --assembly %target% --task %--task% --verbose true
 
