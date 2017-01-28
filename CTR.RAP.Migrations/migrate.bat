f9ad15abf44db21d606eae692e46a46c0175e655
javaday <javaday@gmail.com>
2017-01-28 09:29:42 -0700

    Updated the migration batch

--- CTR.RAP.Migrations/migrate.bat
+++ CTR.RAP.Migrations/migrate.bat
@@ -14,6 +14,37 @@ IF "%--task%"=="" (SET --task=migrate)
 SET migrator="%parent%..\packages\FluentMigrator.1.6.2\tools\migrate.exe"
 SET target="%parent%bin\%--env%\CTR.RAP.Migrations.dll"
 
-%migrator% --conn "Data Source=(local);Initial Catalog=RAP#Cleanup & Total Restoration;Integrated Security=true;" --provider sqlserver2014 --assembly %target% --task %--task% --verbose true
-%migrator% --conn "Data Source=(local);Initial Catalog=RAP#RAP Training;Integrated Security=true;" --provider sqlserver2014 --assembly %target% --task %--task% --verbose true
-
+for %%d in (
+    "RAP#Dummy"
+    "RAP#Maxons"
+    "RAP#Giertsens"
+    "RAP#Cleanup & Total Restoration"
+    "RAP#Kowalski"
+    "RAP#Catastrophe Services Inc"
+    "RAP#Parker Young"
+    "RAP#Sharp, Robins & Popwell"
+    "RAP#VanDam and Krusinga Building and Restoration"
+    "RAP#Carroll Solutions"
+    "RAP#Demo Company 100"
+    "RAP#Baxter Restoration"
+    "RAP#Woodard Cleaning and Restoration"
+    "RAP#ARS Services"
+    "RAP#Cera Restoration"
+    "RAP#Carrara"
+    "RAP#Mike test"
+    "RAP#Disaster One"
+    "RAP#Shane Cunningham"
+    "RAP#HarenLaughlin Restoration"
+    "RAP#Purofirst of Metropolitan Washington"
+    "RAP#South River Restoration (MD)"
+    "RAP#Concraft Inc."
+    "RAP#Harbro"
+    "RAP#JR Johnson"
+    "RAP#RAP Training"
+    "RAP#KB Construction"
+    "RAP#ARS Restoration Specialists"
+    "RAP#Enviro Clean Restore"
+    "RAP#ARS Cleanup Restore Rebuild"
+) do (
+    %migrator% --conn "Data Source=(local);Initial Catalog=%%~d;Integrated Security=true;" --provider sqlserver2014 --assembly %target% --task %--task% --verbose true
+)
