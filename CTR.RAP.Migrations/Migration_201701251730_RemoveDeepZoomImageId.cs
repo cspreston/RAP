ae9e48832ff7277d804514eff68e48694783657f
javaday <javaday@gmail.com>
2017-02-17 07:57:19 -0700

    Created migration to remove DeepZoomImageId field

---
+++ CTR.RAP.Migrations/Migration_201701251730_RemoveDeepZoomImageId.cs
@@ -0,0 +1,27 @@
+﻿
+using FluentMigrator;
+
+namespace CTR.RAP.Migrations
+{
+    [Migration(201701251730)]
+    public class Migration_201701251730_RemoveDeepZoomImageId : Migration
+    {
+        public override void Up()
+        {
+            Delete.ForeignKey("FK_BuildingPlan_Files_ZoomFileId")
+                .OnTable("BuildingPlan");
+
+            Delete.Column("PlanZoomFileId")
+                .FromTable("BuildingPlan");
+        }
+
+        public override void Down()
+        {
+            Alter.Table("BuildingPlan")
+                .AddColumn("PlanZoomFileId")
+                .AsString(128)
+                .Nullable()
+                .ForeignKey("FK_BuildingPlan_Files_ZoomFileId", "Files", "Id");
+        }
+    }
+}
