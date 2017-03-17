084a70e9581423b041fba8a48446e2eb76280318
javaday <javaday@gmail.com>
2017-03-17 03:06:35 -0600

    Added EF Migration

---
+++ Tkw.Repository.Sql/Migrations/201703170903451_RemoveDeepZoomInageId.cs
@@ -0,0 +1,16 @@
+namespace Repository.Sql
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RemoveDeepZoomInageId : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
